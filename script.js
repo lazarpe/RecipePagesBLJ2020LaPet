2cb08b3b8ad0035680a57e7f7735982020bf42fa
lazarpe <lazar.petrovic@noseryoung.ch>
2021-01-22 15:53:53 +0100

    js function removed
    Because I cant explain it

--- script.js
+++ script.js
@@ -1,16 +1,2 @@
-  $(function() {
-    $('.serving').bind('keyup', function(event) {
-        var previousValue = parseFloat($("#previousServing").val());
-        var newValue = parseFloat($(event.target).val());
-        if (previousValue && newValue) {
-            $('.ingredient').each(function(index, elem) {
-                var ingredientNow = $('.amount', elem);
-                var oldIngredientAmount = ingredientNow.text();
-                var newIngredientAmount = oldIngredientAmount * newValue / previousValue;
-                ingredientNow.text(newIngredientAmount);
-            });
-            $('#previousServing').val(newValue);
-        }
-    });
-});
+
   
